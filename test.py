048bb5394d4fc8f1db0d5c4a4283eaf56338c563
Diate <radiobuon9@gmail.com>
2023-02-11 20:24:16 +0700

    kk

---
+++ test.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from keras import models
+import matplotlib.pyplot as plt
+mnist = tf.keras.datasets.mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images = training_images.reshape(training_images.shape[0], 28, 28, 1)
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+save_model = models.load_model("acc9786.h5")
+plt.imshow(test_images[7])
+plt.show()
+resule = save_model.predict(test_images[7:8])
+final = np.argmax(resule)
+#print(test_images[6:7])
+print(resule)
+print(final)
